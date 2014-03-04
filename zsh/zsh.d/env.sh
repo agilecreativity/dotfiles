4aca5f4d4fc252ca952f860e66936480abe7bab7
Burin Choomnuan <agilecreativity@gmail.com>
2014-03-04 19:12:54 +1100

    Update and cleanup the config.

--- zsh/zsh.d/env.sh
+++ zsh/zsh.d/env.sh
@@ -1,23 +1,20 @@
-# See: http://www.cs.elte.hu/zsh-manual/zsh_16.html
-# set default editor to Vim
+## See: http://www.cs.elte.hu/zsh-manual/zsh_16.html
+## set default editor to Vim
 export EDITOR=vi
 export VISUAL=vi
 
-# bind the key to Vi for Zsh instead of Emacs!
-# See:
+## bind the key to Vi for Zsh instead of Emacs!
 # http://dougblack.io/words/zsh-vi-mode.html
 # http://zsh.sourceforge.net/Intro/intro_10.html
 bindkey -v
 
-## experiment settings:BEG: {{{
+## experiment settings {{{
 bindkey '^P' up-history
 bindkey '^N' down-history
 bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 bindkey '^r' history-incremental-search-backward
-## }}}
-
 ## use 10ms for key sequences instead of 0.4 seconds
 export KEYTIMEOUT=1
 ## }}}
@@ -37,18 +34,25 @@ export PATH=$HOME/Dropbox/dotfiles/scripts:$PATH
 export PATH=$HOME/apps/bin:$PATH
 export PATH=$HOME/bin:$PATH
 export PATH=$HOME/.bin:$PATH
-## }}}
 
-# Tips: To temporary clear the zsh history uncomment the next two lines {{{
+## Tips: To temporary clear the zsh history uncomment the next two lines {{{
 #HISTSIZE=5
 #SAVEHIST=5
-# }}}
+## }}}
 
-# Make the ls command look prettier with solarized color scheme
+## Linux specific settings {{{
 if [[ $(uname) == "Linux" ]]; then
   eval `dircolors ~/dotfiles/dircolors/dircolors.256dark`
   #eval `dircolors ~/dotfiles/dircolors/dircolors.ansi-dark`
 fi
+## }}}
+
+## OSX specific settings {{{
+if [[ $(uname) == "Darwin" ]]; then
+  export PATH=$HOME/apps/wkhtml2pdf/bin:$PATH
+  export PATH=/usr/texbin:$PATH
+fi
+## }}}
 
 ## Elixir related settings {{{
 export PATH=$SPIKES_HOME/erlang-projects/elixir/bin:$PATH
