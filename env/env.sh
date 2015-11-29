7c8e93292eb71d381920ad0081dd19970bcdeeaf
Burin Choomnuan <agilecreativity@gmail.com>
2015-11-29 21:35:22 +1100

    Try Emacs key-binding

--- env/env.sh
+++ env/env.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
+## Let's try Emacs
+export EDITOR="emacs -nw"
+export VISUAL="emacs -nw"
+
+## Experiment settings {{{
+## Bind the key to Vi for Zsh instead of Emacs!
 ## See: http://www.cs.elte.hu/zsh-manual/zsh_16.html
 ## http://dougblack.io/words/zsh-vi-mode.html
 ## http://zsh.sourceforge.net/Intro/intro_10.html
+##uncomment below lines if you like to use Vim
 
-export EDITOR=vi
-export VISUAL=vi
-
-## Let's try Emacs
-#export EDITOR="emacs -nw"
-#export VISUAL="emacs -nw"
-
-## Bind the key to Vi for Zsh instead of Emacs!
+#export EDITOR=vi
+#export VISUAL=vi
 #bindkey -v
-
-## Experiment settings {{{
-# bindkey '^P' up-history
-# bindkey '^N' down-history
-# bindkey '^?' backward-delete-char
-# bindkey '^h' backward-delete-char
-# bindkey '^w' backward-kill-word
-# bindkey '^r' history-incremental-search-backward
+#bindkey '^P' up-history
+#bindkey '^N' down-history
+#bindkey '^?' backward-delete-char
+#bindkey '^h' backward-delete-char
+#bindkey '^w' backward-kill-word
+#bindkey '^r' history-incremental-search-backward
 
 ## Use 10ms for key sequences instead of 0.4 seconds
 #export KEYTIMEOUT=1
-# }}}
+## }}}
 
-# Fix the vim's color when running under tmux
+## Fix the vim's color when running under tmux
 export TERM=screen-256color
 
-# Don't clear screen after a command (better with `git status`)
+## Don't clear screen after a command (better with `git status`)
 export LESS=-RX
