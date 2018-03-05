07f898169ed41ec90e0aa0767f21f70e3fcb8fe3
Burin Choomnuan <agilecreativity@gmail.com>
2018-03-05 08:06:45 -0500

    Cleanup aliases in dotfiles

--- env/aliases.sh
+++ env/aliases.sh
@@ -1,19 +1,21 @@
 ## Quick navigation {{{
 alias g-desktop='cd ~/Desktop/'
-alias g-private-dotfiles='cd ~/apps/private-dotfiles/'
+alias g-pr-dotfiles='cd ~/apps/private-dotfiles/'
 alias g-dotfiles='cd ~/apps/dotfiles/'
 alias g-download='cd ~/Downloads/'
 alias g-dropbox='cd ~/Dropbox/'
 alias g-scripts='cd ~/apps/pro-scripts/'
-alias g-tmux-starter='cd ~/apps/tmux-starter/'
-alias g-sp='cd ~/codes/others/spikes/'
+#alias g-sp='cd ~/codes/others/spikes/'
+#alias g-tmux-starter='cd ~/apps/tmux-starter/'
 ## }}}
 
 # Useful aliases {{{
-alias v-als='vim ~/apps/dotfiles/env/aliases.sh'
-alias v-als-priv='vim ~/apps/private-dotfiles/env/aliases.sh'
-alias v-env='vim ~/apps/dotfiles/env/env.sh'
-alias v-env-pr='vim ~/apps/private-dotfiles/env/env.sh'
+alias v-a-pub='vim ~/apps/dotfiles/env/aliases.sh'
+alias v-e-pub='vim ~/apps/dotfiles/env/env.sh'
+
+alias v-a='vim ~/apps/private-dotfiles/env/aliases.sh'
+alias v-e='vim ~/apps/private-dotfiles/env/env.sh'
+
 alias v='vim'
 alias vn='vim -c "NERDTree"'
 alias vt='vim -c "CommandT"'
@@ -32,10 +34,10 @@ alias w3m='noglob w3m -no-mouse'
 alias wget='noglob wget'
 
 # Useful bundle aliases
-alias be=' bundle exec'
-alias ber='bundle exec rake'
-alias bes='bundle exec rspec'
-alias bec='bundle exec cucumber'
+# alias be=' bundle exec'
+# alias ber='bundle exec rake'
+# alias bes='bundle exec rspec'
+# alias bec='bundle exec cucumber'
 
 # When working with Vagrant
 alias ssh-vagrant='ssh deploy@33.33.33.33'
@@ -47,6 +49,9 @@ alias gd2='git diff HEAD~2'
 alias gd3='git diff HEAD~3'
 alias gd4='git diff HEAD~4'
 alias gd5='git diff HEAD~5'
+alias gd6='git diff HEAD~6'
+alias gd7='git diff HEAD~7'
+alias gd8='git diff HEAD~8'
 
 ## Skip non-ascii files
 alias gn1='file $(git diff --name-only HEAD~1) | grep -i ASCII | cut -d: -f1'
@@ -54,6 +59,9 @@ alias gn2='file $(git diff --name-only HEAD~2) | grep -i ASCII | cut -d: -f1'
 alias gn3='file $(git diff --name-only HEAD~3) | grep -i ASCII | cut -d: -f1'
 alias gn4='file $(git diff --name-only HEAD~4) | grep -i ASCII | cut -d: -f1'
 alias gn5='file $(git diff --name-only HEAD~5) | grep -i ASCII | cut -d: -f1'
+alias gn6='file $(git diff --name-only HEAD~6) | grep -i ASCII | cut -d: -f1'
+alias gn7='file $(git diff --name-only HEAD~7) | grep -i ASCII | cut -d: -f1'
+alias gn8='file $(git diff --name-only HEAD~8) | grep -i ASCII | cut -d: -f1'
 
 ## Skip non-ascii files
 alias vd1='vim `file $(git diff --name-only HEAD~1) | grep -i ASCII | cut -d: -f1`'
@@ -61,12 +69,18 @@ alias vd2='vim `file $(git diff --name-only HEAD~2) | grep -i ASCII | cut -d: -f
 alias vd3='vim `file $(git diff --name-only HEAD~3) | grep -i ASCII | cut -d: -f1`'
 alias vd4='vim `file $(git diff --name-only HEAD~4) | grep -i ASCII | cut -d: -f1`'
 alias vd5='vim `file $(git diff --name-only HEAD~5) | grep -i ASCII | cut -d: -f1`'
+alias vd6='vim `file $(git diff --name-only HEAD~6) | grep -i ASCII | cut -d: -f1`'
+alias vd7='vim `file $(git diff --name-only HEAD~7) | grep -i ASCII | cut -d: -f1`'
+alias vd8='vim `file $(git diff --name-only HEAD~8) | grep -i ASCII | cut -d: -f1`'
 
 alias gr1='git rebase -i HEAD~1'
 alias gr2='git rebase -i HEAD~2'
 alias gr3='git rebase -i HEAD~3'
 alias gr4='git rebase -i HEAD~4'
 alias gr5='git rebase -i HEAD~5'
+alias gr6='git rebase -i HEAD~6'
+alias gr7='git rebase -i HEAD~7'
+alias gr8='git rebase -i HEAD~8'
 
 alias gaa='git add --all ./'
 alias gca='git commit -am 'WIP''
@@ -81,8 +95,8 @@ alias git-where-was-i='git log --no-merges --author=$USER'
 alias t-kill='tmux kill-session -t'
 
 ## See: http://usevim.com/2012/03/21/git-and-vimdiff/
-alias v-changed='vim -p `git diff --name-only | grep -v Gemfile`'
-alias run-playbook='ansible-playbook --vault-password-file ~/.vault_pass.txt -vvvv'
+#alias v-changed='vim -p `git diff --name-only | grep -v Gemfile`'
+#alias run-playbook='ansible-playbook --vault-password-file ~/.vault_pass.txt -vvvv'
 ## }}}
 
 ## Platform specific settings {{{
