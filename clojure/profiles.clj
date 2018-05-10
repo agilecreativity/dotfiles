d9077308685c4fe74a2dbc78cdac4746dd91d0e7
Burin Choomnuan <agilecreativity@gmail.com>
2018-05-09 23:40:48 -0400

    Add lein-nevam for pom.xml to project.clj

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,2 +1,5 @@
 {:user {:plugins [[lein-localrepo "0.5.3"]
-                  [lein-try "0.4.3"]]}}
+                  [lein-try "0.4.3"]
+                  ;; Convert pom.xml to project.clj
+                  ;; Note: from https://github.com/thickey/lein-nevam
+                  [lein-nevam "0.1.2"]]}}
