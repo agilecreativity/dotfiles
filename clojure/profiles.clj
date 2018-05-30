dc8892dfac35b3539c8dbf988355f90356db801b
Burin Choomnuan <agilecreativity@gmail.com>
2018-05-30 13:55:30 -0400

    Update lein-localrepo to 0.5.4

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[lein-localrepo "0.5.3"]
+;; See: https://github.com/kumarshantanu/lein-localrepo
+{:user {:plugins [[lein-localrepo "0.5.4"]
                   [lein-try "0.4.3"]
                   ;; Convert pom.xml to project.clj
                   ;; Note: from https://github.com/thickey/lein-nevam
