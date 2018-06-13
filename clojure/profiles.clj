38dace907c57deda15e79c876cbdd88370f474c2
Burin Choomnuan <agilecreativity@gmail.com>
2018-06-13 10:02:10 -0400

    Update and use some cool leiningen plugins

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -3,4 +3,10 @@
                   [lein-try "0.4.3"]
                   ;; Convert pom.xml to project.clj
                   ;; Note: from https://github.com/thickey/lein-nevam
-                  [lein-nevam "0.1.2"]]}}
+                  [lein-nevam "0.1.2"]
+                  ;; https://github.com/venantius/ultra
+                  [venantius/ultra "0.5.2"]]
+        ;; Experimental
+        :dependencies [[pjstadig/humane-test-output "0.8.3"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
