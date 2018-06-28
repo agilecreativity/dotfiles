cceb64e0e55f52424d7ab83aacfff7431d6e4c85
Burin Choomnuan <agilecreativity@gmail.com>
2018-06-28 09:59:44 -0400

    Update some config and switch theme

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,11 +1,16 @@
 ;; See: https://github.com/kumarshantanu/lein-localrepo
-{:user {:plugins [[lein-localrepo "0.5.4"]
+{:user {:plugins [;; Try new api quickly using the repl
                   [lein-try "0.4.3"]
+                  ;;[lein-localrepo "0.5.4"]
                   ;; Convert pom.xml to project.clj
                   ;; Note: from https://github.com/thickey/lein-nevam
                   [lein-nevam "0.1.2"]
+                  ;; A Leiningen plugin for a superior development environment
+                  ;; See: [Introduction Blog Post](https://blog.venanti.us/ultra/)
                   ;; https://github.com/venantius/ultra
-                  [venantius/ultra "0.5.2"]]
+                  [venantius/ultra "0.5.2"]
+                  ;; Keep adding more stuff here
+                  ]
         ;; Experimental
         :dependencies [[pjstadig/humane-test-output "0.8.3"]]
         :injections [(require 'pjstadig.humane-test-output)
