17107870c1a248a4c295be3ce17060f54dfdf3e8
Burin Choomnuan <agilecreativity@gmail.com>
2019-02-22 23:07:42 -0500

    Add more setup for alembic

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,7 +1,6 @@
 ;; See: https://github.com/kumarshantanu/lein-localrepo
 {:user {:plugins [;; Try new api quickly using the repl
                   [lein-try "0.4.3"]
-
                   ;;[lein-localrepo "0.5.4"]
 
                   ;; Convert pom.xml to project.clj
@@ -15,8 +14,13 @@
 
                   ;; Pretty-print a representation of the project map
                   [lein-pprint "1.2.0"]
-                  ;; Keep adding more stuff here
-                  ]
+
+                  ;; For quick typing
+                  [com.palletops/lein-shorthand "0.4.0"]]
+
+        ;; See: https://github.com/pallet/alembic
+        :shorthand {. [alembic.still/distill alembic.still/lein]}
+
         ;; Experimental
         :dependencies [[pjstadig/humane-test-output "0.8.3"]
                        ;; See: https://github.com/pallet/alembic
