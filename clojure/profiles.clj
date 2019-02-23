ed3258a603024f23eac6913b9367912fa9792677
Burin Choomnuan <agilecreativity@gmail.com>
2019-02-22 23:33:32 -0500

    Fix Clojure profiles

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,29 +1,26 @@
-;; See: https://github.com/kumarshantanu/lein-localrepo
-{:user {:plugins [;; Try new api quickly using the repl
-                  [lein-try "0.4.3"]
-                  ;;[lein-localrepo "0.5.4"]
+{:user
+  {:plugins [;;Try new api quickly using the repl
+             ;;[lein-try "0.4.3"]
 
-                  ;; Convert pom.xml to project.clj
-                  ;; Note: from https://github.com/thickey/lein-nevam
-                  [lein-nevam "0.1.2"]
+             ;;=> https://github.com/kumarshantanu/lein-localrepo
+             ;;[lein-localrepo "0.5.4"]
 
-                  ;; A Leiningen plugin for a superior development environment
-                  ;; See: [Introduction Blog Post](https://blog.venanti.us/ultra/)
-                  ;; https://github.com/venantius/ultra
-                  ;;[venantius/ultra "0.5.2"]
+             ;;Convert pom.xml to project.clj
+             ;;[lein-nevam "0.1.2"]
 
-                  ;; Pretty-print a representation of the project map
-                  [lein-pprint "1.2.0"]
+             ;;A Leiningen plugin for a superior development environment
+             ;;[Introduction Blog Post](https://blog.venanti.us/ultra/)
+             ;;=> https://github.com/venantius/ultra
+             ;;[venantius/ultra "0.5.2"]
 
-                  ;; For quick typing
-                  [com.palletops/lein-shorthand "0.4.0"]]
+             ;;=> Pretty-print a representation of the project map
+             [lein-pprint "1.2.0"]
 
-        ;; See: https://github.com/pallet/alembic
-        :shorthand {. [alembic.still/distill alembic.still/lein]}
+             ;;=> https://github.com/pallet/alembic
+             [com.palletops/lein-shorthand "0.4.0"]]
+   :shorthand {. [alembic.still/distill alembic.still/lein]}
+   :dependencies [[alembic "0.3.2"]
+                  [pjstadig/humane-test-output "0.8.3"]]
 
-        ;; Experimental
-        :dependencies [[pjstadig/humane-test-output "0.8.3"]
-                       ;; See: https://github.com/pallet/alembic
-                       [alembic "0.3.2"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+   :injections [(require 'pjstadig.humane-test-output)
+                (pjstadig.humane-test-output/activate!)]}}
