7a5213a1220c2fac516e04c20f4806cadac92907
Burin Choomnuan <agilecreativity@gmail.com>
2019-05-21 23:16:13 -0400

    Add latest version of nrepl for Clojure dev
    - cider-jack-in and rock the world

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,8 +1,9 @@
 {:user
-  {:plugins [[lein-localrepo "0.5.4"] ;; https://github.com/kumarshantanu/lein-localrepo
-             [lein-nevam "0.1.2"]     ;; Convert pom.xml to project.clj
-             [lein-pprint "1.2.0"]]   ;; Pretty-print a representation of the project map
-   :dependencies [[alembic "0.3.2"]
-                  [pjstadig/humane-test-output "0.8.3"]]
-   :injections [(require 'pjstadig.humane-test-output)
-                (pjstadig.humane-test-output/activate!)]}}
+ {:plugins [[lein-localrepo "0.5.4"] ;; https://github.com/kumarshantanu/lein-localrepo
+            [lein-nevam "0.1.2"]     ;; Convert pom.xml to project.clj
+            [lein-pprint "1.2.0"]    ;; Pretty-print a representation of the project map
+            [nrepl "0.6.0"]]
+  :dependencies [[alembic "0.3.2"]
+                 [pjstadig/humane-test-output "0.8.3"]]
+  :injections [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]}}
