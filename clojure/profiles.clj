ab539b001cff13139b3e9d711107f9ce23ebc8eb
Burin Choomnuan <agilecreativity@gmail.com>
2019-05-25 17:28:58 -0400

    Fix ~/.lein/profiles.clj
    - Don't add cider/nrepl as Cider take care of it now :)

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,8 +1,7 @@
 {:user
  {:plugins [[lein-localrepo "0.5.4"] ;; https://github.com/kumarshantanu/lein-localrepo
             [lein-nevam "0.1.2"]     ;; Convert pom.xml to project.clj
-            [lein-pprint "1.2.0"]    ;; Pretty-print a representation of the project map
-            [nrepl "0.6.0"]]
+            [lein-pprint "1.2.0"]]   ;; Pretty-print a representation of the project map
   :dependencies [[alembic "0.3.2"]
                  [pjstadig/humane-test-output "0.8.3"]]
   :injections [(require 'pjstadig.humane-test-output)
