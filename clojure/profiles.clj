0bdaf375c2832b5924873bdae12c903d91612ba9
Burin Choomnuan <agilecreativity@gmail.com>
2020-02-21 22:13:56 -0500

    Update and cleanup

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,9 +1,3 @@
-{:user
- {:plugins [#_[lein-localrepo "0.5.4"] ;; https://github.com/kumarshantanu/lein-localrepo
-            #_[lein-nevam "0.1.2"]     ;; Convert pom.xml to project.clj
-            [lein-pprint "1.2.0"]]     ;; Pretty-print a representation of the project map
-  :dependencies [[alembic "0.3.2"]
-                 #_[pjstadig/humane-test-output "0.8.3"]]
-  #_:injections
-  #_[(require 'pjstadig.humane-test-output)
-     (pjstadig.humane-test-output/activate!)]}}
+;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
+{:user {:plugins [[lein-pprint "1.3.2"]]
+        :dependencies [[alembic "0.3.2"]]}}
