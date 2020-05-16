74a2017dd80c2c94324ca1eabd93292e6963b3c8
Burin Choomnuan <agilecreativity@gmail.com>
2020-05-16 11:57:58 -0400

    Add clj-kondo to default profile
    - lein clj-kondo --lint src ## for linting

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,3 +1,5 @@
 ;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
 {:user {:plugins [[lein-pprint "1.3.2"]]
-        :dependencies [[alembic "0.3.2"]]}}
+        :dependencies [[alembic "0.3.2"]
+                       [clj-kondo "RELEASE"]]
+        :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]}}}
