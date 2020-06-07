df92d989c7511131e2c4451f293cc4e547c28f15
Burin Choomnuan <agilecreativity@gmail.com>
2020-06-07 14:05:21 -0400

    Add antq for outdated deps check
    - For Clojure project

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,5 +1,7 @@
 ;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
 {:user {:plugins [[lein-pprint "1.3.2"]]
         :dependencies [[alembic "0.3.2"]
-                       [clj-kondo "RELEASE"]]
-        :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]}}}
+                       [clj-kondo "RELEASE"]
+                       [antq "RELEASE"]]
+        :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
+                  "outdated"  ["run" "-m" "antq.core"]}}}
