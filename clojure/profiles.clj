d8be352a45d2c9813005d73687bb1ef4ef18ddf9
Burin Choomnuan <burin.choomnuan@gmail.com>
2021-01-18 13:01:15 -0500

    Turn on alembic and add duo-config.edn to ~/.aws

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,7 +1,7 @@
 ;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
 {:user {:plugins [[lein-pprint "1.3.2"
                    lein-ancient "0.6.5"]]
-        :dependencies [#_[alembic "0.3.2"]
+        :dependencies [[alembic "0.3.2"]
                        [clj-kondo "RELEASE"]
                        [antq "RELEASE"]]
         :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
