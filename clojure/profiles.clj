e4d1903a8867570a00331ded45168c371abb6300
Burin Choomnuan <agilecreativity@gmail.com>
2021-01-24 10:42:44 -0500

    Add lein-cloverage to all projects

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,8 +1,9 @@
 ;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
-{:user {:plugins [[lein-pprint "1.3.2"
-                   lein-ancient "0.6.5"]]
-        :dependencies [[alembic "0.3.2"]
+{:user {:plugins [[lein-pprint    "1.3.2"
+                   lein-ancient   "0.6.5"
+                   lein-cloverage "1.2.2"]]
+        :dependencies [[alembic   "0.3.2"]
                        [clj-kondo "RELEASE"]
-                       [antq "RELEASE"]]
+                       [antq      "RELEASE"]]
         :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
                   "outdated"  ["run" "-m" "antq.core"]}}}
