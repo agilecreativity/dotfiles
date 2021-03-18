ba6fde68fb1f13acc067cf7a57f22f88913287c5
Burin Choomnuan <agilecreativity@gmail.com>
2021-03-18 17:01:55 -0400

    Update Clojure tools

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,6 +1,6 @@
 ;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
 {:user {:plugins [[lein-pprint    "1.3.2"
-                   lein-ancient   "0.6.5"
+                   lein-ancient   "0.7.0"
                    lein-cloverage "1.2.2"]]
         :dependencies [[alembic   "0.3.2"]
                        [clj-kondo "RELEASE"]
