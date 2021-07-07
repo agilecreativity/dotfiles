42f5311b472784aab233b1463cddce317c88948e
Burin Choomnuan <agilecreativity@gmail.com>
2021-07-07 14:53:47 -0400

    Add configuration for scope-capture for Clojure

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,9 +1,11 @@
 ;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
-{:user {:plugins [[lein-pprint    "1.3.2"
-                   lein-ancient   "0.7.0"
+{:user {:plugins [[lein-pprint "1.3.2"
+                   lein-ancient "0.7.0"
                    lein-cloverage "1.2.2"]]
-        :dependencies [[alembic   "0.3.2"]
+        :dependencies [[alembic "0.3.2"]
                        [clj-kondo "RELEASE"]
-                       [antq      "RELEASE"]]
+                       [antq "RELEASE"]
+                       [vvvvalvalval/scope-capture "0.3.2"]]
+        :injections [(require 'sc.api)]
         :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
                   "outdated"  ["run" "-m" "antq.core"]}}}
