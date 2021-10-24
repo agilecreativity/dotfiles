d28d211b2cf02d0b5f3b327054643f600a97ee62
Burin Choomnuan <agilecreativity@gmail.com>
2021-10-23 20:06:14 -0400

    Add common plugins for leiningen project

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,11 +1,17 @@
 ;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
-{:user {:plugins [[lein-pprint "1.3.2"
-                   lein-ancient "0.7.0"
-                   lein-cloverage "1.2.2"]]
-        :dependencies [[alembic "0.3.2"]
-                       [clj-kondo "RELEASE"]
-                       [antq "RELEASE"]
-                       [vvvvalvalval/scope-capture "0.3.2"]]
-        :injections [(require 'sc.api)]
-        :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
-                  "outdated"  ["run" "-m" "antq.core"]}}}
+{:user
+ {:plugins
+  [[cider/cider-nrepl "0.25.9"]
+   [lein-ancient "0.7.0"]
+   [lein-check-namespace-decls "1.0.4"]
+   [lein-cljfmt "0.8.0"]
+   [lein-nsorg "0.3.0"]
+   [refactor-nrepl/refactor-nrepl "3.0.0-alpha13"]]}
+  :dependencies
+   [#_[alembic "0.3.2"]
+    [clj-kondo "RELEASE"]
+    [antq "RELEASE"]
+    [vvvvalvalval/scope-capture "0.3.2"]]
+  :injections [(require 'sc.api)]
+  :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
+            "outdated"  ["run" "-m" "antq.core"]}}
