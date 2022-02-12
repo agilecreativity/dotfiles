d93c9cf1d4b6f6c2929d308d0ce1c9c169d8ff64
Burin Choomnuan <agilecreativity@gmail.com>
2022-02-12 15:40:47 +1100

    Update config for Clojure projects

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,13 +1,13 @@
 ;; https://github.com/technomancy/leiningen/blob/stable/doc/PROFILES.md
 {:user
  {:plugins
-  [[cider/cider-nrepl "0.27.2"]
+  [[cider/cider-nrepl "0.27.4"]
    [lein-ancient "1.0.0-RC3"]
    [lein-check-namespace-decls "1.0.4"]
    [lein-cljfmt "0.8.0"]
    [lein-nsorg "0.3.0"]
-   [nrepl "0.9.0-beta3"]
-   [refactor-nrepl "3.0.0-alpha13"]]}
+   [nrepl "0.9.0"]
+   [refactor-nrepl "3.1.0"]]}
   :dependencies
    [#_[alembic "0.3.2"]
     [clj-kondo "RELEASE"]
