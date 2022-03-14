730a5b915599efab286156acfdfdc3517bd92432
Burin Choomnuan <agilecreativity@gmail.com>
2022-03-14 14:37:18 +1100

    Update clojure config

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -5,14 +5,13 @@
    [lein-ancient "1.0.0-RC3"]
    [lein-check-namespace-decls "1.0.4"]
    [lein-cljfmt "0.8.0"]
-   [lein-nsorg "0.3.0"]
+   #_[lein-nsorg "0.3.0"]
    [nrepl "0.9.0"]
    [refactor-nrepl "3.1.0"]]}
   :dependencies
    [#_[alembic "0.3.2"]
     [clj-kondo "RELEASE"]
-    [antq "RELEASE"]
-    [vvvvalvalval/scope-capture "0.3.2"]]
+    [antq "RELEASE"] ]
   :injections [(require 'sc.api)]
   :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
             "outdated"  ["run" "-m" "antq.core"]}}
